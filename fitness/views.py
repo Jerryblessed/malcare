1d7c68272f8e5184831cc5292fc7774e7222edfb
Jeremiah Ope <opejeremiah@gmail.com>
2025-03-03 09:12:41 +0100

    malcare update

--- fitness/views.py
+++ fitness/views.py
@@ -3,25 +3,30 @@ from .models import FitnessActivity, DietaryLog, WeightEntry, UserProfile
 from .forms import UserRegisterForm, ActivityForm, DietaryLogForm, WeightEntryForm
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
 
+# write the activity list view
 @login_required
 def activity_list(request):
     activities = FitnessActivity.objects.filter(user=request.user)
     return render(request, 'fitness/activity_list.html', {'activities': activities})
 
+# write the login view
 def custom_logout(request):
     logout(request)
     return redirect('login')
 
+# write the 404 view
 def custom_404(request, exception):
     return render(request, 'fitness/404.html', status=404)
 
+# write the activity log view
 @login_required
 def diet_log(request):
     diet_logs = DietaryLog.objects.filter(user=request.user)
     return render(request, 'fitness/diet_log.html', {'diet_logs': diet_logs})
 
-
+# write the register view
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
@@ -33,17 +38,25 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'fitness/register.html', {'form': form})
 
-
+# write the home view
 def home(request):
     if request.user.is_authenticated:
         return render(request, 'fitness/home.html')
     return render(request, 'fitness/landing.html')  
 
 
+# write the about view
 @login_required
 def malcare_doc(request):
-    return render(request, 'fitness/malcare.html')
+    context = {
+        "api_key": settings.API_KEY,
+        "azure_openai_endpoint": settings.AZURE_OPENAI_ENDPOINT,
+        "azure_openai_api_key": settings.AZURE_OPENAI_API_KEY,
+        "azure_openai_deployment": settings.AZURE_OPENAI_DEPLOYMENT
+    }
+    return render(request, 'fitness/malcare.html', context)
 
+# add activity view
 @login_required
 def add_activity(request):
     if request.method == 'POST':
@@ -58,6 +71,7 @@ def add_activity(request):
 
     return render(request, 'fitness/add_activity.html', {'form': form})
 
+# add diet log view
 @login_required
 def add_diet_log(request):
     if request.method == 'POST':
@@ -71,7 +85,7 @@ def add_diet_log(request):
         form = DietaryLogForm()
     return render(request, 'fitness/add_diet_log.html', {'form': form})
 
-
+# weight tracker view
 @login_required
 def weight_tracker(request):
     if request.method == 'POST':
@@ -95,6 +109,7 @@ def weight_tracker(request):
         'weights': weights
     })
 
+# login view
 @login_required
 def profile(request):
     user_profile = UserProfile.objects.get(user=request.user)
@@ -104,3 +119,5 @@ def profile(request):
     }
     return render(request, 'fitness/profile.html', context)
 
+
+# a function to view 
